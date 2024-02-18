3f25a3296393c9ccb765b6636fe073e8c30ba5b8
Deonkilladi <110452222+Deonkilladi@users.noreply.github.com>
2024-02-18 20:16:40 +0530

    Add files via upload

---
+++ add two numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a , b ,c;
+    printf("Enter Your First Number",);
+    scanf("%d",&a);
+    printf("Enter Your Second Number ",);
+    scanf("%d",&b);
+    c=a+b
+    printf("The sum is %d",c);
+    return 0;
+}
